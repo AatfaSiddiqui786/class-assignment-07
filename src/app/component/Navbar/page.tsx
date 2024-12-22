e93057b989a4fbdc4ae0ad1a575ddd75d10efabc
AATFA SIDDIQUI <aatfasiddiqui@gmail.com>
2024-12-23 02:08:43 +0500

    Aatfa 7th Assigment

--- src/app/component/Navbar/page.tsx
+++ src/app/component/Navbar/page.tsx
@@ -3,10 +3,13 @@ import Link from 'next/link';
 const Navbar= () => {
   return (
     
-    <div className="min-h-screen bg-gray-200 p-8">
+    <div className="min-h-screen flex  flex-col justify-center bg-black items-center">
     
+    
+    
+    <h1 className="text-4xl text-white font-bold text-center mb-8">Aatfa Siddiqui</h1>
 
-      <h1 className="text-4xl font-bold text-center mb-8">Data Fetching in Next.js</h1>
+      <h1 className="text-4xl font-bold text-white text-center mb-8">Data Fetching in Next.js</h1>
       <div className="flex justify-center space-x-8">
         <Link href="/component/ClientSide">
           <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200">
